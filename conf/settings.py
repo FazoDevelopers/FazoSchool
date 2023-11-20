dbe980275a886d0a85df736c5eca4a245e9a60d3
Ilyosxon0000 <mahmudovilyosxon9@gmail.com>
2023-11-20 05:31:23 +0000

    Commit

--- conf/settings.py
+++ conf/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +24,7 @@ SECRET_KEY = 'django-insecure-s0m@ij=z=e8n2x+obp#95ib4r5n@4*i29i9f(@cdzj+jdq6r1#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DEPLOY=False
+DEPLOY=True
 
 ALLOWED_HOSTS = ["*"]
 
